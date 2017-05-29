5eaf4784ec99288c42705ef002fd719dd29657c2
dhuangdeveloper <dhuang0413@gmail.com>
2017-05-29 23:35:41 +0000

    lesson functions

--- lesson_functions.py
+++ lesson_functions.py
@@ -11,6 +11,24 @@ def convert_color(img, conv='RGB2YCrCb'):
     if conv == 'RGB2LUV':
         return cv2.cvtColor(img, cv2.COLOR_RGB2LUV)
 
+def convert_RGB_to_color_space(image, color_space):
+    """ convert RGB img to a new color space """
+    if color_space != 'RGB':
+        if color_space == 'HSV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        elif color_space == 'LUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)
+        elif color_space == 'HLS':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+        elif color_space == 'YUV':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+        elif color_space == 'YCrCb':
+            feature_image = cv2.cvtColor(image, cv2.COLOR_RGB2YCrCb)
+    else:
+        feature_image = np.copy(image) 
+    return feature_image
+        
+        
 def get_hog_features(img, orient, pix_per_cell, cell_per_block, 
                         vis=False, feature_vec=True):
     # Call with two outputs if vis==True
@@ -79,20 +97,19 @@ def extract_features(images, color_space='RGB', spatial_size=(32, 32),
             hist_features = color_hist(feature_image, nbins=hist_bins)
             file_features.append(hist_features)
         if hog_feat == True:
-        # Call get_hog_features() with vis=False, feature_vec=True
-            if hog_channel == 'ALL':
-                hog_features = []
-                for channel in range(feature_image.shape[2]):
-                    hog_features.append(get_hog_features(feature_image[:,:,channel], 
-                                        orient, pix_per_cell, cell_per_block, 
-                                        vis=False, feature_vec=True))
-                hog_features = np.ravel(hog_features)        
-            else:
-                hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
-                            pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-            # Append the new feature vector to the features list
-            file_features.append(hog_features)
-        features.append(np.concatenate(file_features))
+            #hog_features = [get_hog_features(feature_image[:,:,w],
+            #                                 orient, pix_per_cell, cell_per_block,
+            #                                 vis=False, feature_vec=False)
+            #                for w in hog_channel]
+            #for channel in range(feature_image.shape[2]):
+            #    hog_features.append(get_hog_features(feature_image[:,:,channel], 
+            #                                         orient, pix_per_cell, cell_per_block, 
+            #                                         vis=False, feature_vec=False))
+            file_features.append(np.ravel([get_hog_features(feature_image[:,:,w],
+                                             orient, pix_per_cell, cell_per_block,
+                                             vis=False, feature_vec=False)
+                                           for w in hog_channel]))
+            features.append(np.concatenate(file_features))
     # Return list of feature vectors
     return features
 
@@ -220,17 +237,58 @@ def single_img_features(img, color_space='RGB', spatial_size=(32, 32),
         img_features.append(hist_features)
     #7) Compute HOG features if flag is set
     if hog_feat == True:
-        if hog_channel == 'ALL':
-            hog_features = []
-            for channel in range(feature_image.shape[2]):
-                hog_features.extend(get_hog_features(feature_image[:,:,channel], 
-                                    orient, pix_per_cell, cell_per_block, 
-                                    vis=False, feature_vec=True))      
-        else:
-            hog_features = get_hog_features(feature_image[:,:,hog_channel], orient, 
-                        pix_per_cell, cell_per_block, vis=False, feature_vec=True)
-        #8) Append features to list
-        img_features.append(hog_features)
+        
+
+        hog_features = [get_hog_features(feature_image[:,:,w],
+                                                      orient, pix_per_cell, cell_per_block,
+                                                      vis=False, feature_vec=True)
+                                     for w in hog_channel]                
+        img_features.append(np.ravel(hog_features))
 
     #9) Return concatenated array of features
     return np.concatenate(img_features)
+
+
+def add_heat(heatmap, bbox_list):
+    # Iterate through list of bboxes
+    for box in bbox_list:
+        # Add += 1 for all pixels inside each bbox
+        # Assuming each "box" takes the form ((x1, y1), (x2, y2))
+        heatmap[box[0][1]:box[1][1], box[0][0]:box[1][0]] += 1
+
+    # Return updated heatmap
+    return heatmap
+
+def apply_threshold(heatmap, threshold):
+    # Zero out pixels below the threshold
+    heatmap[heatmap <= threshold] = 0
+    # Return thresholded map
+    return heatmap
+
+def get_labeled_bboxes(img, labels):
+    bbox = np.zeros(label[1], 4)
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox[car_number,:] = np.array([np.min(nonzerox), np.min(nonzeroy), np.max(nonzerox), np.max(nonzeroy)])        
+    return bbox
+    
+
+def draw_labeled_bboxes(img, labels):
+    # Iterate through all detected cars
+    for car_number in range(1, labels[1]+1):
+        # Find pixels with each car_number label value
+        nonzero = (labels[0] == car_number).nonzero()
+        # Identify x and y values of those pixels
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+        # Define a bounding box based on min/max x and y
+        bbox = ((np.min(nonzerox), np.min(nonzeroy)), (np.max(nonzerox), np.max(nonzeroy)))
+        # Draw the box on the image
+        cv2.rectangle(img, bbox[0], bbox[1], (0,0,255), 6)
+    # Return the image
+    return img
